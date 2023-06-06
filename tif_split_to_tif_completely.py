c6c54560c175f73a4a8dc55c509cf8ac159cee1d
yiguolu <yiguolu@qq.com>
2023-06-06 17:51:54 +0600

    增加进度条

--- tif_split_to_tif_completely.py
+++ tif_split_to_tif_completely.py
@@ -1,6 +1,7 @@
 import os
 import rasterio
 import numpy as np
+from tqdm import tqdm
 
 """用于将大的tif文件切割成小的tif文件,包括原图和标签, 原图为4通道, 标签为单通道
     1. 可调节切割尺寸
@@ -18,7 +19,7 @@ def read_tif(tif_image_path: str):
 
     # rasterio 打开的图像对象好像只能一个波段一个波段的读取数据
     # 这个循环是把这些波段的np数组堆叠起来
-    for band_count in range(1, big_tif.count+1):
+    for band_count in tqdm(range(1, big_tif.count+1)):
         if band_count == 1:
             big_tif_array = big_tif.read(band_count)
         elif band_count == 2:
@@ -29,8 +30,8 @@ def read_tif(tif_image_path: str):
                 (big_tif_array, np.expand_dims(big_tif.read(band_count), 0)))
 
     # shape:(channels, height, width)
-    print(
-        f"shape:{big_tif_array.shape}, max:{big_tif_array.max()}, \nunique:{np.unique(big_tif_array)}")
+    # print(
+    #     f"shape:{big_tif_array.shape}, max:{big_tif_array.max()}, \nunique:{np.unique(big_tif_array)}")
     return big_tif_array
 
 
@@ -59,7 +60,7 @@ def split_tif(tif_array: np.ndarray, stride: int, crop_size: tuple):
 
     # 开始数组切片, 滑动窗口, 以 stride 为步长,
     # 从左到右, 从上到下, 切出大小为 (crop_height, crop_width) 的小图
-    for y in range(0, height+1, stride):
+    for y in tqdm(range(0, height+1, stride)):
         for x in range(0, width+1, stride):
             if x+crop_width <= width and y+crop_height <= height:
                 if len(tif_array.shape) == 3:
@@ -108,10 +109,9 @@ def save_array_as_tif(matrix, path, profile=None, prototype=None):
             dst.write(matrix, 1)
 
 
-def main(big_tif_image_path:str, big_tif_mask_path:str, crop_size:tuple, stride:int):
+def main(big_tif_image_path:str, crop_size:tuple, stride:int):
     # 输入路径
     big_tif_image_path = big_tif_image_path
-    big_tif_mask_path = big_tif_mask_path
 
     # 图片裁剪尺寸
     image_size = crop_size
@@ -121,19 +121,14 @@ def main(big_tif_image_path:str, big_tif_mask_path:str, crop_size:tuple, stride:
 
     # 读取tif为numpy数组
     array_image = read_tif(big_tif_image_path)
-    array_mask = read_tif(big_tif_mask_path)
 
     # 分割numpy数组为numpy数组list
     image_tif_array_list = split_tif(array_image, stride, image_size)
-    mask_tif_array_list = split_tif(array_mask, stride, image_size)
 
     # 输出路径
     save_image_path_root = f"images_{image_size[0]}"
     if not os.path.exists(save_image_path_root):
         os.makedirs(save_image_path_root)
-    save_mask_path_root = f"masks_{image_size[0]}"
-    if not os.path.exists(save_mask_path_root):
-        os.makedirs(save_mask_path_root)
 
     # 保存tif文件(原图)
     metadata = {
@@ -147,35 +142,17 @@ def main(big_tif_image_path:str, big_tif_mask_path:str, crop_size:tuple, stride:
         metadata["width"] = tif_array.shape[2]
         metadata["height"] = tif_array.shape[1]
         save_array_as_tif(tif_array, os.path.join(
-            save_image_path_root, f"{index:0>4d}.tif"), metadata)
+            save_image_path_root, f"{index:0>6d}.tif"), metadata)
         print("\r保存进度(原图):{}/{}:{}".format(index+1, len(image_tif_array_list), "▋"*(int((index+1)/len(image_tif_array_list)*100)//2)),
               "%{:.1f}".format((index+1)/len(image_tif_array_list)*100),
               flush=True,
               end="")
         
-    # 保存tif文件(标签)
-    metadata = {
-        'driver': 'GTiff',
-        'width': image_size[1],
-        'height': image_size[0],
-        'count': 1,
-        'dtype': np.uint8
-    }
-    for index, tif_array in enumerate(mask_tif_array_list):
-        metadata["width"] = tif_array.shape[1]
-        metadata["height"] = tif_array.shape[0]
-        save_array_as_tif(tif_array, os.path.join(
-            save_mask_path_root, f"{index:0>4d}.tif"), metadata)
-        print("\r保存进度(标签):{}/{}:{}".format(index+1, len(mask_tif_array_list), "▋"*(int((index+1)/len(mask_tif_array_list)*100)//2)),
-              "%{:.1f}".format((index+1)/len(mask_tif_array_list)*100),
-              flush=True,
-              end="")
 
 
 if __name__ == "__main__":
     # 输入路径
-    big_tif_image_path = "tif_image\splited_image_and_mask\image_part1.tif"
-    big_tif_mask_path = "tif_image\splited_image_and_mask\mask_part1.tif"
+    big_tif_image_path = "tif_image/GF6_PMS_E85.4_N44.6_20220514_L1A1120209504-MUX_fuse.tif"
 
     # 图片裁剪尺寸
     crop_size = (256, 256)
@@ -184,7 +161,6 @@ if __name__ == "__main__":
     stride = 256
 
     main(big_tif_image_path=big_tif_image_path,     # 原图路径
-         big_tif_mask_path=big_tif_mask_path,       # 标签路径
          crop_size=crop_size,                       # 裁剪尺寸
          stride=stride)                             # 裁剪步长
 
